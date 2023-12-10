cb46e8dd618764bf2e9227c452abcae8b40ab624
shajib1927 <shajibshahrear@gmail.com>
2023-12-10 15:43:27 +0300

    route-updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './global.css'
+import { Route, Routes } from 'react-router-dom'
+import Signinform from './_auth/forms/Signinform'
+import {Home} from './_root/pages/Home'
+import Signupform from './_auth/forms/Signupform'
+import Authlayout from './_auth/Authlayout'
+import Rootlayout from './_root/Rootlayout'
 
 const App = () => {
   return (
-    <div>App</div>
+    <main className='flex h-screen'>
+      <Routes>
+        
+        {/* public routes */}
+        <Route element={<Authlayout/>} >
+          <Route path='/sign-in' element = {<Signinform/>} />
+          <Route path='/sign-in' element = {<Signupform/>} />
+        </Route>
+        
+        {/* private routes */}
+        <Route element={<Rootlayout/>} >
+          <Route index element = {<Home/>} />
+        </Route>
+        
+      </Routes>
+    </main>
   )
 }
 
