178a0935303192d171b930aa45a3c3d3200446a7
shajib1927 <shajibshahrear@gmail.com>
2023-12-10 22:14:19 +0300

    sign methods updated with Loader

--- src/_auth/forms/Signupform.tsx
+++ src/_auth/forms/Signupform.tsx
@@ -12,10 +12,11 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { SignupValidation } from "@/lib/validation";
+import Loader from "@/components/shared/Loader";
+import { Link } from "react-router-dom";
 
 const Signupform = () => {
-
-  const isLoading = true;  
+  const isLoading = 0;
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
@@ -56,7 +57,9 @@ const Signupform = () => {
               <FormItem>
                 <FormLabel className="shad-form_label">Name</FormLabel>
                 <FormControl>
-                  <Input type="text" className="shad-input" {...field} />
+                  <Input type="text" 
+                  placeholder="Enter your name"
+                  className="shad-input" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -70,7 +73,9 @@ const Signupform = () => {
               <FormItem>
                 <FormLabel className="shad-form_label">Username</FormLabel>
                 <FormControl>
-                  <Input type="text" className="shad-input" {...field} />
+                  <Input type="text"
+                  placeholder="Enter username"
+                  className="shad-input" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -84,7 +89,9 @@ const Signupform = () => {
               <FormItem>
                 <FormLabel className="shad-form_label">Email</FormLabel>
                 <FormControl>
-                  <Input type="text" className="shad-input" {...field} />
+                  <Input type="text"
+                  placeholder="Enter Email"
+                  className="shad-input" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -98,13 +105,32 @@ const Signupform = () => {
               <FormItem>
                 <FormLabel className="shad-form_label">Password</FormLabel>
                 <FormControl>
-                  <Input type="password" className="shad-input" {...field} />
+                  <Input type="password" 
+                  placeholder="Set Password : at least 8 chars"
+                  className="shad-input" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <Button type="submit" className="shad-button_primary">{isLoading ? "Loading..." : Submit}</Button>
+          <Button type="submit" className="shad-button_primary">
+            {isLoading ? (
+              <div className="flex-center gap-2">
+                <Loader /> Loading...
+              </div>
+            ) : (
+              "Submit"
+            )}
+          </Button>
+
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Already have an account?
+            <Link
+              to="/sign-in"
+              className="text-primary-500 text-small-semibold ml-1">
+              Log in
+            </Link>
+          </p>
         </form>
       </div>
     </Form>
