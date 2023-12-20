c115c12c0821b7e37b256a656985d82fc668d413
shajib1927 <shajibshahrear@gmail.com>
2023-12-20 06:38:49 +0300

    appwrite updated

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -3,16 +3,16 @@ import { Client, Account, Databases, Storage, Avatars } from "appwrite";
 
 const appwriteConfig = {
     projectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    url: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-//   databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
-//   storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
-//   userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
-//   postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
-//   savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
+    url: String(import.meta.env.VITE_APPWRITE_URL),
+    databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+    storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+    userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
+    postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
+    savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
 };
 
-console.log(appwriteConfig.projectId);
-console.log(appwriteConfig.url);
+// console.log(appwriteConfig.projectId);
+// console.log(appwriteConfig.url);
 
 
 export const client = new Client();
@@ -21,8 +21,8 @@ export const client = new Client();
 // client.setEndpoint('https://cloud.appwrite.io/v1');
 // client.setProject('65760f29c0e065608ea7');
 
-// client.setEndpoint(appwriteConfig.url);
-// client.setProject(appwriteConfig.projectId);
+client.setEndpoint(appwriteConfig.url);
+client.setProject(appwriteConfig.projectId);
 
 
 export const account = new Account(client);
